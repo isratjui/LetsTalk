c29f2712f612e1a3ee77b79eeec64d3e38892cfe
Israt Jahan Jui <jahanjuiisrat@gmail.com>
2020-04-02 22:46:47 +0600

    More Added

---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php
+//DB Params
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_NAME", "lalaland");
+
+define("SITE_TITLE", "Welcome To lalaland!");
+
+//Paths
+define ('BASE_URI', 'http://'.$_SERVER['SERVER_NAME'].'/talkingspace/');
